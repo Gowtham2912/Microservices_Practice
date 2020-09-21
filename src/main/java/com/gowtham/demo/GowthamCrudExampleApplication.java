a23d0e3a262ef3a4edf5587c2cbc3fca760c4dbf
Gowtham2912 <69888695+Gowtham2912@users.noreply.github.com>
2020-09-21 18:38:04 +0530

    Add files via upload

---
+++ src/main/java/com/gowtham/demo/GowthamCrudExampleApplication.java
@@ -0,0 +1,16 @@
+package com.gowtham.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class GowthamCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowthamCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowtham/demo/GowthamCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowthamCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
